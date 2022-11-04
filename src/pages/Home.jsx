14c00e597b326161661ea6578bd7c4af1cacafe7
Pawan-Strivemindz <pawan@strivemindz.com>
2022-11-04 19:01:35 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Announcement from '../componentns/Announcement'
+import Navbar from '../componentns/Navbar'
+import Slider from '../componentns/Slider'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Announcement/>
+        <Navbar/>
+        <Slider/>
+    </div>
+  )
+}
+
+export default Home
